e4de1632e025bf53b7d76650f56d98c28d4deea2
Hel0bed0 <10gamerastro@gmail.com>
2026-03-09 13:54:35 +0000

    added .py script
    req python3

---
+++ ConvertyCtoKpyscript.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+
+ # Function that converts C to K
+ 
+ #Loop Script
+o = 1
+ 
+while(o == 1):
+ 
+ # User Input
+    n = float(input("Enter Celsius Value (will crash if NaN):  "))
+    #Crash if NaN
+    if n == NaN:
+      exit()
+
+    c = 1
+
+ # Convert
+    def CelsiusTokelvin (c):
+        return n + 273.15
+
+    y = CelsiusTokelvin (c)
+
+    
+
+ # Print Answer
+    print (f"{n} Celsius is equal {y} Kelvin")
+
+
