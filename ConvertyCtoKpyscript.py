451a01e29f94642ee0ee17a072edd3e8d5824064
Helobedo <10gamerastro@gmail.com>
2026-06-29 16:24:57 +0100

    v1.3
    added exit function
    optimized code

--- ConvertyCtoKpyscript.py
+++ ConvertyCtoKpyscript.py
@@ -4,13 +4,11 @@
  # Function that converts C to K
  
  #Loop Script
-o = 1
- 
-while(o == 1):
+while True:
  
  # User Input
     n = float(input("Enter Celsius Value (will crash if NaN):  "))
-     
+
     c = 1
 
  # Convert
@@ -22,7 +20,10 @@ while(o == 1):
     
 
  # Print Answer
-    print (f"{n} Celsius is equal to {y} Kelvin")
-    print ("Method: added 273.15")
-
+    print (f"{n} Celsius is equal to {y} Kelvin\n"
+           "Method: added 273.15\n")
+ #ask to exit
+    ex = int(input("1 to exit, other to not:  "))
+    if ex == 1:
+        break
 
